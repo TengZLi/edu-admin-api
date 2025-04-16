1a764ad16dc90b1a58e36c1bae2d281593755da6
tomli6668 <tom.li6668@gmail.com>
2025-04-16 22:19:27 +0800

    路由表

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\OmisePaymentController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// 公共路由
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+// Omise支付回调
+Route::post('/payment/webhook', [OmisePaymentController::class, 'handleWebhook']);
+Route::get('/payment/callback', [OmisePaymentController::class, 'paymentCallback'])->name('payment.callback');
+
+// 教师路由组
+Route::middleware('auth:teacher')->prefix('teacher')->group(function () {
+    // 用户信息
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::put('/profile', [AuthController::class, 'updateProfile']);
+
+    // 课程管理
+    Route::prefix('course')->group(function () {
+        Route::get('/', [CourseController::class, 'index']);
+        Route::post('/', [CourseController::class, 'create']);
+        Route::get('/{id}', [CourseController::class, 'show']);
+        Route::put('/{id}', [CourseController::class, 'update']);
+    });
+    // 获取学生列表
+    Route::get('/students', [StudentController::class, 'list']);
+    // 账单管理
+    Route::prefix('invoice')->group(function () {
+        Route::get('/', [InvoiceController::class, 'index']);
+        Route::post('/', [InvoiceController::class, 'create']);
+        Route::post('/{id}/send', [InvoiceController::class, 'send']);
+    });
+});
+
+// 学生路由组
+Route::middleware('auth:student')->prefix('student')->group(function () {
+    // 用户信息
+
+    // Omise支付
+    Route::post('/invoice/{id}/pay-with-omise', [OmisePaymentController::class, 'payWithOmise']);
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::put('/profile', [AuthController::class, 'updateProfile']);
+
+    // 我的课程
+    Route::get('/course', [CourseController::class, 'studentCourses']);
+
+    // 我的账单
+    Route::prefix('invoice')->group(function () {
+        Route::get('/', [InvoiceController::class, 'studentInvoices']);
+        Route::post('/{id}/pay', [InvoiceController::class, 'pay']);
+    });
 });
